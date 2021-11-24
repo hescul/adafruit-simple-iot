13a3fc03152ffe70a518f8916b94c2d459ec4110
Minh Nguyen <61007865+hescul@users.noreply.github.com>
2021-11-24 12:25:16 +0700

    hide username and password

--- mobile/app/src/main/java/com/hescul/adafruitdb/BrokerConfig.java
+++ mobile/app/src/main/java/com/hescul/adafruitdb/BrokerConfig.java
@@ -4,6 +4,6 @@ public class BrokerConfig {
     static String AUTH_CONTEXT = "ssl";
     static final String HOST_NAME = "io.adafruit.com";
     static final String HOST_PORT = "8883";
-    static final String USERNAME  = "_angelus";
-    static final String PASSWORD  = "aio_MkfU397QoXQKt6ZGeG6BMbZGksG1";
+    static final String USERNAME  = "****";
+    static final String PASSWORD  = "****";
 }
